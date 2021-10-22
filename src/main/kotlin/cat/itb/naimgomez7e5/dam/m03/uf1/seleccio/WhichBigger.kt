54c1af6773e77073adb119882b94b4f96fea84fe
naim.gomez.7e5 <naimgomezcn@gmail.com>
2021-10-22 11:27:33 +0200

    test

---
+++ src/main/kotlin/cat/itb/naimgomez7e5/dam/m03/uf1/seleccio/WhichBigger.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main()
+{
+    var a = Scanner(System.`in`).nextInt()
+    var b = Scanner(System.`in`).nextInt()
+
+    if(a > b)
+    {
+        print(a)
+    }
+    else
+    {
+        print(b)
+    }
+
+}
