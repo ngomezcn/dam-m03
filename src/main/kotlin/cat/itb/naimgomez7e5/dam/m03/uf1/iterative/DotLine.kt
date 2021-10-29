e547243b035456b3f547b8baa589736be42eaed9
naim.gomez.7e5 <naim.gomez.7e5@itb.cat>
2021-10-29 10:43:00 +0200

    xd

---
+++ src/main/kotlin/cat/itb/naimgomez7e5/dam/m03/uf1/iterative/DotLine.kt
@@ -0,0 +1,11 @@
+package cat.itb.naimgomez7e5.dam.m03.uf1.iterative
+
+import java.util.*
+
+fun main()
+{
+    for(i in 1..(Scanner(System.`in`).nextInt()))
+    {
+        print("·")
+    }
+}
---
+++ src/main/kotlin/cat/itb/naimgomez7e5/dam/m03/uf1/iterative/HowManyLines.kt
@@ -0,0 +1,20 @@
+package cat.itb.naimgomez7e5.dam.m03.uf1.iterative
+
+import java.util.*
+
+fun main()
+{
+    var counter = 0;
+    var line = ""
+
+    while (line != "END")
+    {
+        counter += 1
+        line = Scanner(System.`in`).nextLine();
+    }
+
+
+    print(counter-1)
+}
+
+
