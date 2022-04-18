929adc09e1e3b749a194b4765c2b3504ff14f9f7
ngomezcn <naimgomezcn@gmail.com>
2022-04-18 20:45:52 +0200

    zzz

---
+++ src/main/kotlin/cat/itb/naimgomez7e5/dam/m03/uf4/exercices/intro/ThreeRectanglesOneLeftPiramid.kt
@@ -0,0 +1,38 @@
+package cat.itb.naimgomez7e5.dam.m03.uf4.exercices.intro
+
+import cat.itb.naimgomez7e5.dam.m03.uf4.exercices.intro.figures.*
+/*
+class RectangleFigure(val color: String, val width: Int, val height: Int) {
+    fun paint() {
+        repeat (width) {
+            repeat (height) {
+                print(color+"X"+RESET)
+            }
+            println()
+        }
+    }
+}
+
+class LeftPiramidFigure(val color: String, val base: Int) {
+    fun paint() {
+
+        for (i in 0 until base+1 ) {
+            print(color)
+            for (j in 0 until i) {
+                print('X')
+            }
+            println(RESET)
+        }
+        println()
+    }
+}
+
+fun main() {
+    var rectangleFigure1 = RectangleFigure(RED, 4, 5)
+    var leftPiramidFigure = LeftPiramidFigure(YELLOW, 3)
+    var rectangleFigure2 = RectangleFigure(GREEN, 3, 5)
+
+    rectangleFigure1.paint()
+    leftPiramidFigure.paint()
+    rectangleFigure2.paint()
+}*/
